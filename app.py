6e61411bd188f3821517b7db897e516291bc890e
Grimsas <grimsas@users.noreply.github.com>
2020-11-30 18:18:08 +0000

    new post sort off works now and lots of other changes

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import os
+import json
 from flask import (Flask, flash, redirect, render_template, request, url_for)
 from flask_pymongo import PyMongo
 from flask_wtf import FlaskForm
 # from flask_uploads import UploadSet, IMAGES
 from bson.objectid import ObjectId
+from bson import json_util
+from bson.json_util import dumps
 from wtforms import (StringField, PasswordField,
-                     BooleanField, TextAreaField, SelectField)
+                     BooleanField, TextAreaField, SelectField, SubmitField)
 from wtforms.validators import InputRequired, Length, EqualTo
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from flask_login import (LoginManager, UserMixin,
                          login_user, login_required, logout_user, current_user)
 from werkzeug.security import generate_password_hash, check_password_hash
-# from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename
 if os.path.exists('env.py'):
     import env
 
@@ -26,7 +29,6 @@ mongo = PyMongo(app)
 login_manager = LoginManager(app)
 
 login_manager.login_view = 'login'
-login_manager.login_message = u'Please log in to access this page.'
 
 # images = UploadSet('images', IMAGES)
 
@@ -62,15 +64,26 @@ class SignupForm(FlaskForm):
     confirm = PasswordField('Repeat Password')
 
 class ReviewForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired()])
-    category = SelectField('Category', choices=[(
-        'first', 'First'), ('second', 'Second'), ('third', 'Third')])
+    # username = current_user.user_session['username']
+    category = SelectField('Category',
+                           choices=list(mongo.db.categories.find()))
     brand = StringField('Brand', validators=[InputRequired()])
-    review = TextAreaField('Your Story')
+    review = TextAreaField('Your Story', validators=[InputRequired()])
     accept_tnc = BooleanField('I accept the T&C',
                               validators=[InputRequired()]),
+    title = StringField('Title', validators=[InputRequired()])
     # upload = FileField('image', FileAllowed(images, 'Images only!'))
 
+class ReviewFormTags(FlaskForm):
+    # username = current_user.user_session['username']
+    category = SelectField('Category',
+                           choices=list(mongo.db.categories.find()))
+    brand = StringField('Brand', validators=[InputRequired()])
+    review = TextAreaField('Your Story', validators=[InputRequired()])
+    accept_tnc = BooleanField('I accept the T&C',
+                              validators=[InputRequired()]),
+    title = StringField('Title', validators=[InputRequired()])
+    # upload = FileField('image', FileAllowed(images, 'Images only!'))
 
 @app.route('/')
 @app.route('/get_products')
@@ -107,12 +120,9 @@ def login():
     if form.validate_on_submit():
         users = mongo.db.users
         user = users.find_one({'username': form.username.data})
-        print(user)
         if user is None or not check_password_hash(
                 user['password'], form.password.data):
             flash('Invalid username or password')
-            print(check_password_hash(
-                user['password'], form.password.data))
             return redirect(url_for('login'))
         auth_user = User(user)
         login_user(auth_user)
@@ -121,6 +131,7 @@ def login():
     return render_template('login.html', form=form, action=action)
 
 @app.route('/profile/<username>', methods=['GET', 'POST'])
+@login_required
 def profile(username):
     username = current_user.user_session['username']
     if username:
@@ -130,18 +141,48 @@ def profile(username):
 
 @app.route('/post_review', methods=['GET', 'POST'])
 def post_review():
+    action = 'post_review'
     post_review = ReviewForm()
+    categories = list(mongo.db.categories.find())
     if post_review.validate_on_submit():
         confim_post = {
             'username': post_review.username.data,
             'caregory': post_review.category.data,
             'brand': post_review.brand.data,
             'review': post_review.review.data,
-            'accept_tnc': post_review.accept_tnc.data,
+            'accept_tnc': post_review.accept_tnc.data
             # 'upload': post_review.upload.data,
         }
         return render_template('confirm_post.html', **confim_post)
-    return render_template('post_review.html', post_review=post_review)
+    return render_template('post_review.html',
+                           post_review=post_review,
+                           action=action, categories=categories)
+
+@app.route('/post_review', methods=['GET', 'POST'])
+def post_review_tags():
+    action = 'post_review_tags'
+    post_review_tags = ReviewFormTags()
+    if post_review_tags.validate_on_submit():
+        tags = {
+            'skintype': ['Normal', 'Oily', 'Dry',
+                         'Conbination', 'Sensitive', 'Mature'],
+            'pigmentation': ['Highly Pigmented', 'High Coverage',
+                             'Medium Coverage', 'Sheer', 'Buildable'],
+            'finish': ['Matte', 'Dewy', 'Satin', 'Glossy'],
+            'suggested_ocasions': ['Normal', 'Oily', 'Dry',
+                                   'Conbination', 'Sensitive', 'Mature']
+        }
+        return render_template('confirm_post.html', **tags)
+    return render_template('post_review.html',
+                           post_review_tags=post_review_tags, action=action)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index(tags):
+    if request.method == 'POST':
+        print(request.form.getlist(tags))
+        return redirect(url_for('get_products'))
+    return render_template('index.html')
 
 
 @app.route('/get_categories')
@@ -149,10 +190,15 @@ def get_categories():
     categories = list(mongo.db.categories.find().sort('category_name', 1))
     return render_template('categories.html', categories=categories)
 
-@app.route('/profile/administrator')
+@app.route('/admin/admin/', methods=['GET', 'POST'])
 @login_required
 def administrator():
-    return '<p>Hello {}</p>'.format(current_user.user_session['username'])
+    username = current_user.user_session['username']
+    if username == 'administrator':
+        return '<p>Hello {}</p>'.format(current_user.user_session['username'])
+    flash('YOU SHALL NOT PASS')
+    return redirect(url_for('get_products'))
+
 
 @app.route('/logout')
 @login_required
